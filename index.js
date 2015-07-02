7e683fa388da57e2fff56d9b184a1a759d8f6476
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-07-01 22:27:42 -0400

    finding tags

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ require('lazy-ass');
 var check = require('check-more-types');
 var Promise = require('bluebird');
 var R = require('ramda');
+var _ = require('lodash');
 var packageJson = Promise.promisify(require('package-json'));
 var packageField = Promise.promisify(require('package-json').field);
+var GitHubApi = require('github');
+
+var github = new GitHubApi({
+  // required
+  version: '3.0.0',
+  // optional
+  debug: false
+});
+
+var log = console.log.bind(console);
 var debug = require('debug')('load');
 
 function compareVersionPackages(from, to) {
@@ -13,7 +24,9 @@ function compareVersionPackages(from, to) {
 }
 
 var question = {
+  user: 'bahmutov',
   name: 'next-update',
+  repo: 'next-update',
   from: '0.8.0',
   to: '0.8.2' // or 'latest'
 };
@@ -39,5 +52,56 @@ function packageRepo() {
     .then(R.prop('url'));
 }
 
-packageRepo()
-  .then(console.log.bind(console));
+// packageRepo()
+//  .then(console.log.bind(console));
+
+
+function getTags(options) {
+  var repoSchema = {
+    user: check.unemptyString,
+    repo: check.unemptyString
+  };
+  la(check.object(options), 'missing options', options);
+  la(check.schema(repoSchema, options), options);
+
+  var gTags = Promise.promisify(github.repos.getTags);
+  return gTags({
+    user: 'bahmutov',
+    repo: 'next-update'
+  }).then(function (tags) {
+    la(check.array(tags), 'expected tags to be an array', tags);
+    console.log('received %d tags', tags.length);
+    return _.map(tags, function (tag) {
+      return {
+        name: tag.name,
+        sha: tag.commit.sha
+      };
+    });
+  });
+}
+
+function getFromToTags(question) {
+  var tagSchema = {
+    from: check.unemptyString,
+    to: check.unemptyString
+  };
+  la(check.schema(tagSchema, question), question);
+
+  return getTags(_.pick(question, 'user', 'repo'))
+    .then(function (allTags) {
+      la(check.array(allTags), 'missing tags', allTags);
+      var fromTag = _.find(allTags, 'name', question.from);
+      la(fromTag, 'cannot find tag', question.from);
+      var toTag = _.find(allTags, 'name', question.to);
+      la(toTag, 'cannot to tag', question.to);
+
+      return _.extend(question, {
+        fromTag: fromTag,
+        toTag: toTag
+      });
+    });
+}
+
+// getTags();
+getFromToTags(question)
+  .then(log);
