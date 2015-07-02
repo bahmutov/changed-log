fbb5a534d7e1788e2449aba2ef51eccc866da300
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-07-02 14:37:47 -0400

    adding --auth options

--- bin/changed-log.js
+++ bin/changed-log.js
@@ -16,15 +16,23 @@ var options = {
   to: process.argv[4]
 };
 
+options.auth = _.some(process.argv, function (word) {
+  return word === '--auth';
+});
+debug('options', options);
+
 var isValidCliOptions = check.schema.bind(null, {
   name: check.unemptyString,
   from: check.unemptyString,
   to: check.unemptyString
 });
 if (!isValidCliOptions(options)) {
-  log('%s@%s <package name> <from version> <to version>', pkg.name, pkg.version);
+  log('%s@%s <package name> <from version> <to version> [options]',
+    pkg.name, pkg.version);
+  log('options:\n  --auth  - login with github credentials for increased rate limit');
   process.exit(-1);
 }
+
 
 var changedLog = require('../src/changed-log');
 changedLog(options);
