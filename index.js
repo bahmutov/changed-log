4332802eff0bfebf099b51aed222eb77fd332210
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-07-02 10:51:56 -0400

    started main algorithm

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 require('lazy-ass');
 var check = require('check-more-types');
-var Promise = require('bluebird');
-var R = require('ramda');
-var _ = require('lodash');
-var utils = require('./src/utils');
 
 var log = console.log.bind(console);
 var debug = require('debug')('main');
 
+var pkg = require('./package.json');
+
+var options = {
+  name: process.argv[2],
+  from: process.argv[3],
+  to: process.argv[4]
+};
+
+var isValidCliOptions = check.schema.bind(null, {
+  name: check.unemptyString,
+  from: check.unemptyString,
+  to: check.unemptyString
+});
+if (!isValidCliOptions(options)) {
+  log('%s@%s <package name> <from version> <to version>', pkg.name, pkg.version);
+  process.exit(-1);
+}
+
+var packageRepo = require('./src/package-repo');
+packageRepo(options.name)
+  .tap(debug);
