a6b1c4dd3f7048acdcd21bc27a0bd05b9e0056ae
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-07 17:28:50 -0500

    test(github): Mocking github response to avoid hitting api limits during unit test
    fixes #19

--- mocks/mock-for-chalk.js
+++ mocks/mock-for-chalk.js
@@ -1,1 +1,31 @@
 console.log('running with mocked Github calls');
+
+require('lazy-ass');
+var check = require('check-more-types');
+var Promise = require('bluebird');
+var mockData = require('./mock-chalk-ids.json');
+
+require = require('really-need');
+
+function mockGetCommitsFromTags(info) {
+  console.log('mock get commits from tags', info);
+  la(info.repo === 'chalk', 'got unexpected request', info);
+  return Promise.resolve(mockData);
+}
+
+require('../src/get-commits-from-tags', {
+  post: function (exported, filename) {
+    return mockGetCommitsFromTags;
+  }
+});
+
+(function adjustProcessArgs() {
+  process.argv.splice(1, 1);
+  console.log('removed this filename from process arguments');
+  process.argv[1] = require('path').resolve(process.argv[1]);
+  console.log('resolved the name of the next script to load');
+}());
+
+console.log('loading the real script from %s', process.argv[1]);
+require(process.argv[1]);
+
